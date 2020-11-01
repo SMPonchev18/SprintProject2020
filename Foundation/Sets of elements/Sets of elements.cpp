76634c9eacdca135803735f819df2b884cd2be79
YAKarakolev18 <YAKarakolev18@codingburgas.bg>
2020-11-01 23:28:39 +0200

    Add SetChoice function

--- Foundation/Sets of elements/Sets of elements.cpp
+++ Foundation/Sets of elements/Sets of elements.cpp
@@ -13,3 +13,17 @@ int LetterCheck() {
 	}
 	return a;
 }
+
+//function that you choose from which set of elements you want to work with
+int SetChoice() 
+{
+	int option;
+	cout << "Enter a valid option: ";
+	option = LetterCheck();
+	while (option < 1 or option > 2) {
+		cout << "\nThere are only 2 sets you can choose from! Try again by typing 1 or 2. 1 is for the first set and 2 is for the second set: ";
+		option = LetterCheck();
+	}
+	return option;
+}
+
